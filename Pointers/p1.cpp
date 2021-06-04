a2a586dd15b7fb27fa9f61d827291a7966f03074
Gourav Arora <gouravarora035@gmail.com>
2021-06-04 18:48:05 +0530

    pointers

---
+++ Pointers/p1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// char a='a';
+	// char *ptr = &a;
+
+	// cout << "a value is" << *ptr;
+	// cout << "adress is" << ptr ;
+
+	int a=10;
+	int *ptr;
+	ptr = &a;
+	cout << *ptr;
+	// int *ptr = &a;
+	// // cout <<*ptr++ << endl;
+	// cout <<*ptr++ << endl;
+	// cout <<*ptr++ << endl;
+}
