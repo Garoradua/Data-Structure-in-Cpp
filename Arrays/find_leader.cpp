75bd3adfa8bc137136d40889d2be33bcb8842509
Gourav Arora <gouravarora035@gmail.com>
2021-06-21 19:19:39 +0530

    array

---
+++ Arrays/find_leader.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+  using namespace std;
+  
+  int main()
+  {
+    //write your code here
+    int t;
+    cin >> t;
+    while(t--){
+     int n;
+      cin >> n;
+      int a[n];
+      int k;
+      for(int i=0; i<n; i++){
+        cin >> a[i];
+      }
+      cout << a[n-1] << " ";
+      for(int i=n-2; i>=0; i--){
+      if(a[i]>a[i+1]){
+        k=0
+        cout << a[i] << " ";
+      }else{
+        k=1;
+        break;
+      }
+       
+        }
+        if(k==0){
+          cout << a[i] << " ";
+        }
+      }
+      cout << endl;
+    }
+    
+    return 0;
+  }
