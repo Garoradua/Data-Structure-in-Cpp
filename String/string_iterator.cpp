7daa513d88ee915d20ef095c34b371481bb5fe5a
Gourav Arora <gouravarora035@gmail.com>
2021-06-28 11:32:34 +0530

    string

--- String/string_iterator.cpp
+++ String/string_iterator.cpp
@@ -4,7 +4,8 @@ int main(){
 	string str1 = "Gourav";
 	string str2 = "Arora";
 
-	string::iterator itr1, itr2;
+	string::iterator itr1;
+	string::reverse_iterator itr2;
 	// string::iterator itr2;
 	// itr1 = str1.begin();
 	// cout << *itr1;
@@ -12,7 +13,14 @@ int main(){
 	// itr2 = str1.begin();
 	// cout << *itr2;
 
+
+	// string from beginning
 	for(itr1 = str1.begin(); itr1!=str1.end(); itr1++){
 		cout << *itr1;
+	}
+
+	// string from reverse
+	for(itr2 = str1.rbegin(); itr2!=str1.rend(); itr2++){
+		cout << *itr2;
 	}
 }
